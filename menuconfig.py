e990e2610047a75a3692eb9cbde6439ed7ff2daa
Lara Maia <lara@craft.net.br>
2013-11-18 12:10:14 -0200

    Use english as global language

--- menuconfig.py
+++ menuconfig.py
@@ -1,14 +1,14 @@
 # coding=utf8
 
 cats = [
-    ["Entwicklung","+Development","applications-development.png"],
-    ["Netzwerk","+Network","applications-internet.png"],
-    ["Büro","+Office","applications-office.png"],
-    ["Spiele","+Games","applications-games.png"],
-    ["Wissenschaft","+Science","applications-science.png"],
-    ["Grafik","+Graphics-AudioVideo","applications-graphics.png"],
+    ["Development","+Development","applications-development.png"],
+    ["Network","+Network","applications-internet.png"],
+    ["Office","+Office","applications-office.png"],
+    ["Games","+Games","applications-games.png"],
+    ["Science","+Science","applications-science.png"],
+    ["Graphics","+Graphics-AudioVideo","applications-graphics.png"],
     ["Multimedia","+AudioVideo-Graphics","applications-multimedia.png"],
     ["System","+System","applications-system.png"],
-    ["Sonstiges","Unmatched","applications-other.png"],
-    ["Einstellungen","+Settings","applications-utilities.png"],
+    ["Others","Unmatched","applications-other.png"],
+    ["Settings","+Settings","applications-utilities.png"],
     ]
